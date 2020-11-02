e757fc8bb9fdd020cda2d6e79f38f669f4427aea
Sara A <calisaraalhindi@gmail.com>
2020-11-02 22:51:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
      :name => "Carl",
      :favorite_food => "fried chicken"
       }
-    params.to_s
+    # params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
 
